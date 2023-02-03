1f9e59af1c61f57d6d6692f50896b645cf1f80d3
Aleksandra Marinovic <63455453+saska0799@users.noreply.github.com>
2023-02-03 17:15:54 +0100

    Delete formatDate.js

--- src/utils/formatDate.js
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date) => {
-  const dateObj = date !== undefined ? new Date(date) : new Date();
-  const month = dateObj.getUTCMonth() + 1;
-  const day = dateObj.getUTCDate();
-  const year = dateObj.getUTCFullYear();
-  return day + "/" + month + "/" + year;
-};
